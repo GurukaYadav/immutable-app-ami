c7fdc29a4444038afb6a8edf7daab465d54868b2
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-18 21:11:21 +0530

    updating immutable-app-ami

---
+++ ec2.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "local.TAG_NAME"
+  }
+}
+
+resource "null_resource" "null" {
+  depends_on = [null_resource.copy-local-artifacts]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+      host     = aws_instance.instance.private_ip
+    }
+
+    inline = [
+      "ansible-pull -U https://github.com/GurukaYadav/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=ENV -e DOCDB_ENDPOINT=DOCDB_ENDPOINT -e REDIS_ENDPOINT=REDIS_ENDPOINT -e RDS_ENDPOINT=RDS_ENDPOINT"
+    ]
+  }
+}
+
+resource "null_resource" "copy-local-artifacts" {
+  provisioner "file" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+      host     = aws_instance.instance.private_ip
+    }
+    source      = "${var.COMPONENT}-${var.APP_VERSION}.zip"
+    destination = "/tmp/${var.COMPONENT}.zip"
+  }
+}
+
+resource "aws_ami_from_instance" "ami" {
+  name               = "${var.COMPONENT}-${var.APP_VERSION}"
+  source_instance_id = "aws_instance.instance.id"
+}
---
+++ security-grp.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "sg" {
+  name        = "${local.TAG_NAME}-sg"
+  description = "Allow TCP inbound traffic"
+
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.TAG_NAME}-sg"
+  }
+
+}
