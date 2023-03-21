f6753e2ee63967f85f720bad3fe00016e01654de
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-21 16:05:03 +0530

    updating immutable-app-ami

--- ec2.tf
+++ ec2.tf
@@ -41,4 +41,8 @@ resource "aws_ami_from_instance" "ami" {
   depends_on         = [null_resource.resource]
   name               = "${var.COMPONENT}-${var.APP_VERSION}"
   source_instance_id = aws_instance.instance.id
+
+  tags = {
+    Name = "${local.TAG_PREFIX}-ami"
+  }
 }
